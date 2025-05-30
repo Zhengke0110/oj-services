85a1caa52e7c4dcac30b87d689415bb79bd7d6c7
zhengke <zhengke996@163.com>
2025-05-30 18:42:51 +0800

    feat(services): 添加在线判题服务模块
    - 新增 oj-judge 模块，实现在线判题功能
    - 添加 Nacos 配置和 Redis 连接配置
    - 实现登录拦截器，用于保护 API 端点
    - 更新项目结构，添加新模块的依赖和配置

---
+++ oj-judge/src/main/java/fun/timu/oj/judge/JudgeApplication.java
@@ -0,0 +1,20 @@
+package fun.timu.oj.judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@EnableFeignClients
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"fun.timu.oj.judge", "fun.timu.oj.common"})
+@SpringBootApplication
+public class JudgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JudgeApplication.class, args);
+    }
+
+}
