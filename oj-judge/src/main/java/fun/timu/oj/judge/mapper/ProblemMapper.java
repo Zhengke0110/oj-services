a7436d24bdca24677bc0eccb148c0506ad7a8b3f
zhengke <zhengke996@163.com>
2025-05-31 12:41:34 +0800

    refactor(mapper): 为 mapper 接口添加 @Mapper 注解
    为 CodeExecutionRecordMapper、ProblemMapper、ProblemTagMapper、
    ProblemTagRelationMapper 和 TestCaseMapper 接口添加了 @Mapper 注解，以明确标识这些接口为 MyBatis 的映射器接口。

--- oj-judge/src/main/java/fun/timu/oj/judge/mapper/ProblemMapper.java
+++ oj-judge/src/main/java/fun/timu/oj/judge/mapper/ProblemMapper.java
@@ -2,6 +2,7 @@ package fun.timu.oj.judge.mapper;
 
 import fun.timu.oj.judge.model.DO.ProblemDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author zhengke
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @createDate 2025-05-30 18:41:57
  * @Entity generator.domain.Problem
  */
+@Mapper
 public interface ProblemMapper extends BaseMapper<ProblemDO> {
 
 }
--- oj-judge/src/main/java/fun/timu/oj/judge/mapper/ProblemTagMapper.java
+++ oj-judge/src/main/java/fun/timu/oj/judge/mapper/ProblemTagMapper.java
@@ -2,6 +2,7 @@ package fun.timu.oj.judge.mapper;
 
 import fun.timu.oj.judge.model.DO.ProblemTagDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author zhengke
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @createDate 2025-05-30 18:41:57
  * @Entity generator.domain.ProblemTag
  */
+@Mapper
 public interface ProblemTagMapper extends BaseMapper<ProblemTagDO> {
 
 }
